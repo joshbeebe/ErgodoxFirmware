c7a456902005cee5f6d8d1e5b6d053b9c5186957
Josh <joshbeebe202@gmail.com>
2018-09-19 19:00:15 -0400

    Added support for multi-key layer operations

--- layer.c
+++ layer.c
@@ -1,6 +1,11 @@
 #include "layer.h"
 #include "libs/usb/usb_keyboard.h"
 #include "libs/mcp/mcp23018.h"
+#include <string.h>
+
+static bool isSecLayerSet = false;
+static bool isSecLayerPushed = false;
+static uint8_t secLayerNumber = 0;
 
 
 /*
@@ -9,10 +14,10 @@
  * lay: char of the number of the layer is to be pushed onto the stack
  *  NOTE: currently only supports layers 0-9. Function will have to be changed to allow for multi-digit
  *        numbers to support more layers
- * isPressed: really only used to keep the layer from being pushed multiple times. 
+ * isPressed: really only used to keep the layer from being pushed multiple times.
  *            Also needed for the function pointer
  */
-void _push_layer(int layer) {
+static inline void _push_layer(int layer) {
     g_stackLength++;
     g_layerStack[g_stackLength] = layer;
 }
@@ -23,9 +28,20 @@ void push_layer(void* lay, bool isPressed) {
         //convert to int since layer is passed in as a character
         layer -= '0';
         _push_layer(layer);
+
+        //Secondary layer
+        if (strlen((char*)lay) > 1) {
+            uint8_t secLayer = ((char*)lay)[1];
+            if (isSecLayerSet && secLayer == secLayerNumber) {
+                _push_layer(secLayer);
+                isSecLayerPushed = true;
+            } else {
+                isSecLayerSet = true;
+                secLayerNumber = secLayer;
+            }
+        }
         //The LED to turn on is the first # of the string
         /*_led_on((((char*)g_keys[layer][0][0].data)[0] - '0'));*/
-        //currentLayer = layerStack[stackLength];
     }
 }
 /*
@@ -34,10 +50,10 @@ void push_layer(void* lay, bool isPressed) {
  * lay: char of number of the layer to remove
  *  NOTE: currently only supports layers 0-9. Function will have to be changed to allow for multi-digit
  *        numbers to support more layers
- * isPressed: really only used to keep the layer from being popped multiple times. 
+ * isPressed: really only used to keep the layer from being popped multiple times.
  *            Also needed for the function pointer
  */
-bool _pop_layer(int layer) {
+static bool _pop_layer(int layer) {
     int i = g_stackLength;
     while (g_layerStack[i] != layer) {
         i--;
@@ -54,7 +70,6 @@ bool _pop_layer(int layer) {
     g_stackLength--;
     return true;
     /*_led_off((((char*)g_keys[layer][0][0].data)[0] - '0'));*/
-    //currentLayer = layerStack[stackLength];
 }
 void pop_layer(void* lay, bool isPressed) {
     int layer = ((char*)lay)[0];
@@ -63,6 +78,17 @@ void pop_layer(void* lay, bool isPressed) {
     //only pop on release of button
     if (!isPressed) {
         _pop_layer(layer);
+
+
+        if (strlen((char*)lay) > 1) {
+            uint8_t secLayer = ((char*)lay)[1];
+            if (isSecLayerPushed && secLayer == secLayerNumber) {
+                _pop_layer(secLayer);
+                isSecLayerPushed = false;
+            } else {
+                isSecLayerSet = false;
+            }
+        }
     }
 }
 void toggle_layer(void* lay, bool isPressed) {
@@ -75,7 +101,7 @@ void toggle_layer(void* lay, bool isPressed) {
 }
 
 //Used to keep track of which keys need to be released when layer is toggled off
-uint8_t num_toggledLayers = 0; 
+uint8_t num_toggledLayers = 0;
 uint8_t num_toggledKeys = 0;
 uint16_t toggledPressedKeys[6] = {0};
 
@@ -86,8 +112,8 @@ uint16_t toggledPressedKeys[6] = {0};
  * lay: char of number to toggle
  * isPressed: whether the layer is pushed or popped. Also needed for function pointer.
  *
- * TODO: determine whether this pushes the layer multiple times. If it does, a 
- *       static bool may need to be used to stop that. That solution would only 
+ * TODO: determine whether this pushes the layer multiple times. If it does, a
+ *       static bool may need to be used to stop that. That solution would only
  *       allow 1 layer to be toggled at a time, though.
  * TODO: only release keys which were pressed while a layer was being toggled
  */
